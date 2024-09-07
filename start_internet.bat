8a3c2aaf409aada6c5091d975c5a121883b45913
Bishan-Pankaja <bishanpankaja4@gmail.com>
2024-09-07 01:22:11 -0700

    Delete start_internet.bat

--- start_internet.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-netsh advfirewall firewall delete rule name=all program="%ProgramFiles%\BlueStacks_nxt\HD-Player.exe"
-netsh advfirewall firewall delete rule name=all program="%ProgramFiles%\BlueStacks\HD-Player.exe"
-netsh advfirewall firewall delete rule name=all program="%ProgramFiles%\BlueStacks_msi2\HD-Player.exe"
-netsh advfirewall firewall delete rule name=all program="%ProgramFiles%\BlueStacks_msi5\HD-Player.exe"
-netsh advfirewall firewall delete rule name=all program="%ProgramData%\BlueStacks_msi5\HD-Player.exe"
-netsh advfirewall firewall delete rule name=all program="%ProgramFiles(x86)%\SmartGaGa\ProjectTitan\Engine\ProjectTitan.exe"
